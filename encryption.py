017ae291373e962eeb431ad4afe983f01909bc5a
Rahul <rahul25.devrhl@gmail.com>
2026-03-04 17:32:36 +0530

    finalized the security class

---
+++ encryption.py
@@ -0,0 +1,118 @@
+# link for reference to learn about this code: https://gemini.google.com/share/153bee9247ba
+
+import os
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import base64
+
+
+class security():
+    """provide the methods for encryptmsg and decryptmsg child classes"""
+
+    def __init__(self):
+        """Do nothing"""
+        pass
+        
+    def generateSalt(self) -> str:
+        """generate and return a new 16 raw byte salt value for each image during encryption"""
+        salt = os.urandom(16) 
+        return salt
+
+    def setMasterPwd(self) -> str:
+        """ask user for master password as input and return the str"""
+        master = input("Enter the Master Password: ")   
+        return master.encode('utf-8')   
+
+    def generateKey(self, mpwd, salt) -> str:
+        """use the salt and masterPwd attribute stored in self to generate and return the key for encryption and decryption"""
+        master_password = mpwd
+        seed_salt = salt
+
+        keyDerivative = PBKDF2HMAC(algorithm=hashes.SHA256(),
+                        length=32,
+                        salt=seed_salt,
+                        iterations=60000)
+
+        key = keyDerivative.derive(master_password)
+        return key
+        
+
+
+class encryptMsg(security):
+
+    def __init__(self, msg:str) -> None:
+        """
+        argument: message to encrypt
+
+        sets up the key using salt and master password for encryption"""
+        self.msg = msg
+        self.masterPwd = self.setMasterPwd()
+        self.salt = self.generateSalt()
+        self.key = self.generateKey(self.masterPwd,self.salt)
+
+    def encrypt(self) -> str:
+        """
+        argument: nothing
+        encrypts the message object and returns the encrypted str ready to be stored in the image"""
+
+        msg = self.msg
+        message = msg.encode('utf-8')
+
+        aesengine =AESGCM(self.key)
+        nonce = os.urandom(12)
+        ciphertext = aesengine.encrypt(nonce,message, None)
+
+        ciphertext_b64str = base64.b64encode(ciphertext).decode('utf-8')
+        salt_b64str = base64.b64encode(self.salt).decode('utf-8')
+        nonce_b64str = base64.b64encode(nonce).decode('utf-8')
+        return "001as".join([salt_b64str, nonce_b64str, ciphertext_b64str])
+    
+
+class decryptMsg(security):
+
+    def __init__(self, extracted:str):
+        """
+        argument: msg to decrypt
+        extracts the salt, nonce and ciphertext from the str extracted from the image and set up the key for decryption"""
+
+        elementlst = extracted.split("001as")
+        salt_b64str = elementlst[0]
+        nonce_b64str = elementlst[1]
+        ciphertext_b64str = elementlst[2]
+
+        self.salt = base64.b64decode(salt_b64str)
+        self.nonce = base64.b64decode(nonce_b64str)
+        self.ciphertext = base64.b64decode(ciphertext_b64str)
+        self.masterPwd = self.setMasterPwd()
+        self.key = self.generateKey(self.masterPwd,self.salt)
+
+    def decrypt(self) -> str:
+        """
+        argument: nothing
+        decrypts and return the decrypted message finally"""
+
+        aesengine = AESGCM(self.key)
+
+        decrypted_msg = aesengine.decrypt(self.nonce, self.ciphertext, None)
+        msg = decrypted_msg.decode('utf-8')
+
+        return msg
+
+
+# message = encryptMsg("hello is it working")
+# encmsg = message.encrypt()
+# print(encmsg)
+
+# todecmsg = decryptMsg(encmsg)
+# messageretrieved = todecmsg.decrypt()
+# print(messageretrieved)
+
+
+        
+    
+
+
+    
+
+
